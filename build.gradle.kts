d5aa0aaef42c6535d2321ebe4e97ad5dadc8c330
putriamelia22 <utticantik101@gmail.com>
2025-12-24 21:23:14 +0700

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// File: build.gradle.kts (Project level)
+
+plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
